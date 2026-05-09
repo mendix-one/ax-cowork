8a25de0002ea25e2a720106b6487a2aec0b26703
Danniel Nguyen <dannielnguyen289@gmail.com>
2026-05-10 00:04:58 +0900

    setup backend concept

--- services/cdn-services/src/main.module.ts
+++ services/cdn-services/src/main.module.ts
@@ -1,10 +1,9 @@
-import {Module} from '@nestjs/common'
-import {ConfigModule} from './acore/config/config.module'
-import {WorkerModule} from './worker/worker.module'
-import {ServicesModule} from "./services/services.module";
+import { Module } from '@nestjs/common'
+import { ConfigModule } from './acore/config/config.module'
+import { WorkerModule } from './worker/worker.module'
+import { ServicesModule } from './services/services.module'
 
 @Module({
   imports: [ConfigModule, WorkerModule, ServicesModule],
 })
-export class MainModule {
-}
+export class MainModule {}
--- services/cdn-services/src/main.ts
+++ services/cdn-services/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core'
 import { ConfigService } from '@nestjs/config'
+import { ValidationPipe } from '@nestjs/common'
 import { NestExpressApplication } from '@nestjs/platform-express'
 
 import { MainModule } from './main.module'
@@ -11,7 +12,27 @@ async function bootstrap() {
   // Config service
   const configService = app.get(ConfigService)
 
+  // Global pipes
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      validateCustomDecorators: true,
+    }),
+  )
+
+  // CORS
+  const options = {
+    origin: '*',
+    methods: ['OPTIONS', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['content-type', 'authorization', 'cdn-owner-id', 'timezone', 'lang'],
+    exposedHeaders: ['authorization', 'code'],
+    optionsSuccessStatus: 200,
+  }
+  app.enableCors(options)
+
   // Start listing
   await app.listen(configService.get<number>('PORT') ?? 3011)
 }
+
 void bootstrap()
